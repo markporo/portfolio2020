137a9c4f0e7861ccfa935a25b7a3458d69d44328
markporo <markporo@yahoo.com>
2020-01-09 14:01:06 -0500

    delay time

--- src/js/sr.js
+++ src/js/sr.js
@@ -2,7 +2,7 @@ export default function() {
   const defaultProps = {
     easing: 'cubic-bezier(0.5, 0, 0, 1)',
     distance: '30px',
-    duration: 1000,
+    duration: 700,
     desktop: true,
     mobile: true
   };
@@ -10,7 +10,7 @@ export default function() {
   /* Section Title */
   ScrollReveal().reveal('.section-title', {
     ...defaultProps,
-    delay: 300,
+    delay: 200,
     distance: '0px',
     origin: 'bottom'
   });
@@ -18,43 +18,43 @@ export default function() {
   /* Hero Section */
   ScrollReveal().reveal('.hero-title', {
     ...defaultProps,
-    delay: 500,
+    delay: 300,
     origin: window.innerWidth > 768 ? 'left' : 'bottom'
   });
   ScrollReveal().reveal('.hero-cta', {
     ...defaultProps,
-    delay: 1000,
+    delay: 700,
     origin: window.innerWidth > 768 ? 'left' : 'bottom'
   });
 
   /* About Section */
   ScrollReveal().reveal('.about-wrapper__image', {
     ...defaultProps,
-    delay: 600,
+    delay: 300,
     origin: 'bottom'
   });
   ScrollReveal().reveal('.about-wrapper__info', {
     ...defaultProps,
-    delay: 1000,
+    delay: 700,
     origin: window.innerWidth > 768 ? 'left' : 'bottom'
   });
 
   /* Projects Section */
   ScrollReveal().reveal('.project-wrapper__text', {
     ...defaultProps,
-    delay: 500,
+    delay: 200,
     origin: window.innerWidth > 768 ? 'left' : 'bottom'
   });
   ScrollReveal().reveal('.project-wrapper__image', {
     ...defaultProps,
-    delay: 1000,
+    delay: 700,
     origin: window.innerWidth > 768 ? 'right' : 'bottom'
   });
 
   /* Contact Section */
   ScrollReveal().reveal('.contact-wrapper', {
     ...defaultProps,
-    delay: 800,
+    delay: 500,
     origin: 'bottom'
   });
 }
